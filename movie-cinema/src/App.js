7e2c2167df1029dfb8deb3d5da5f8c7a4c404761
bgpaten <ahyarpattani@gmail.com>
2024-03-25 10:58:48 +0700

    Praktikum pertemuan 4 - Props & Media

--- movie-cinema/src/App.js
+++ movie-cinema/src/App.js
@@ -1,45 +1,69 @@
-import { Component } from "react";
+import Header from "./component/Header";
+import Car from "./component/Car";
+import User from "./component/User";
+import Footer from "./component/Footer";
+import imgProfil from "./component/about.jpg"
 
 function App() {
   return (
     <div className="App">
       <Header />
       <h1>Hello World</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum velit eos
-        exercitationem culpa modi fugit ullam illo eveniet laboriosam dolorem
-        facere tempore, nostrum a aliquid? Dolorem nesciunt praesentium rerum
-        repellat.
-      </p>
-      <Footer></Footer>
+      <User
+        gambar="https://cdn-icons-png.flaticon.com/512/1077/1077114.png"
+        nama="Asep"
+        alamat="Jaksel"
+        angkatan={11}
+        hobies={["Badminton", "Futsal", "Tenis Meja"]}
+        status={false}
+        profil={(nama) => alert("Profil " + nama + " telah dibuka")}
+      />
+      <User
+        gambar={imgProfil}
+        nama="Samsul"
+        alamat="Jaksel"
+        angkatan={11}
+        hobies={["Badminton", "Futsal", "Tenis Meja"]}
+        status={true}
+        profil={(nama) => alert("Profil " + nama + " telah dibuka")}
+      />
+      <Car
+        merk="Dodge"
+        harga={5}
+        isNew={true}
+        colors={["Hitam", "Putih", "abu-abu"]}
+        beli={() => alert("Sudah dibeli")}
+      />
+      <Footer nama="BgPaten" />
     </div>
   );
 }
 
 // componennt harus huruf kapital di awalnya
 // jika return hanya berisi satu tag, maka tidak memerlukan kurung
-const Header = () => {
-  return (
-    <nav>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Login</li>
-      </ul>
-    </nav>
-  );
-};
+// const Header = () => {
+//   return (
+//     <nav>
+//       <ul>
+//         <li>Home</li>
+//         <li>About</li>
+//         <li>Login</li>
+//       </ul>
+//     </nav>
+//   );
+// };
 
 // cara susah ni
-class Footer extends Component {
-  render() {
-    return (
-      <footer>
-        <h3>Copyright &copy;2024 Developed by BgPaten</h3>
-        <span>Make with &#10084</span>
-      </footer>
-    );
-  }
-}
+// class Footer extends Component {
+//   render() {
+//     return (
+//       <footer>
+//         {/* props atau property agar bisa dipakai dimana-mana */}
+//         <h3>Copyright &copy;2024 Developed by {this.props.nama}</h3>
+//         <span>Make with &#10084</span>
+//       </footer>
+//     );
+//   }
+// }
 
 export default App;
